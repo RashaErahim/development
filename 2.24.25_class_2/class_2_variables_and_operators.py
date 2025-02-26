d588d239ca869199009ab7d214ad6e3506fb7ff3
Your Name <rasho1326@gmail.com>
2025-02-26 16:28:22 -0500

    Multiplicaion complete

---
+++ 2.24.25_class_2/class_2_variables_and_operators.py
@@ -0,0 +1,107 @@
+# Print Built in Function
+
+# Single Line Comment
+
+# Addition
+print(9+9)
+
+# Subtraction
+print(45-9)
+
+
+# Multiplication
+
+
+# Division
+
+
+# Exponents
+
+
+
+# Integer Division
+
+
+# Modulo/Mod/Remainder
+
+
+# Using Variables as placeholders
+
+
+# Program to find the perimeter of a rectangle
+
+
+
+# Data Types
+
+# Integer
+
+
+
+# String
+
+
+
+# Bool
+
+
+# Float
+
+
+# String
+
+
+# List 
+
+
+# For loop
+
+
+# Dictionary
+
+
+# Cast a string to an integer
+
+
+# Cast integer to string
+
+# new_string = str(second_num)
+
+# Colors
+
+
+
+
+# Fahrenheit to Celsius
+# fahrenheit = 89
+
+# Eval
+# cold_weather = 'True'
+# print(eval(cold_weather))
+
+
+'''
+# You are given a triangle with a side of 4, base of 6, and a side of 3. Create
+a brief python script that will determine the perimeter of the triangle. Comment your code
+
+1. Print the perimeter
+2. Using boolean operators is side #1 greater than the base?
+3. Using boolean operators is side #2 less than the base?
+4. Using boolean operators is base larger than or equal to side #1?
+'''
+
+
+
+
+
+
+
+'''
+Write some code that converts fahrenheit to celsius.
+
+To convert fahrenheit to celsius, subtract 32, then multiply by 5/9.
+
+C = (F - 32) * 5/9
+
+
+'''
