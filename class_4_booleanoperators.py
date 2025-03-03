1ec3ff3494171d9ca76cabcae886c95984797730
Your Name <rasho1326@gmail.com>
2025-03-03 18:12:40 -0500

    class_4_boolaan_operator

---
+++ class_4_booleanoperators.py
@@ -0,0 +1,76 @@
+# Boolean Operators
+
+# Is 7 less than 5? <
+seven_less_than_five =7<5
+print(seven_less_than_five)
+
+# Is 4 less than or equal to 4? <=
+
+
+# Is 6 greater than 2? >
+
+
+
+# Is 5 greater than or equal to 6? >=
+
+
+
+# Is 5 equal to 5? ==
+
+
+# AND KEYWORD - Only delivers true if both values are True
+
+
+# Python OR Operator takes at least two boolean expressions and returns True if any one of the expressions is True.
+
+
+# not
+
+# Is x less than y?
+
+
+
+# is - Used to test if variables are the same object
+
+
+
+''' The id() function returns a unique id for the specified object.
+All objects in Python has its own unique id.
+The id is assigned to the object when it is created.'''
+# print(id(first_group))
+# print(id(second_group))
+
+
+x = ['apple', 'banana', 'cherries']
+y = ['apple', 'banana', 'cherries']
+
+
+
+# in
+
+
+# eval
+
+
+# Shortcut Operators
+
+
+
+age = 20
+
+num = 5
+
+
+
+# Program to convert Fahrenheit to Celsius using shortcut operators
+
+
+
+# fahrenheit = 89
+
+
+# celsius = (fahrenheit - 32) * 5/9
+
+
+
+
