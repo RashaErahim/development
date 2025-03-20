2ccbb967e1243f7e884fc454c57ac6bb7bc701ef
Your Name <rasho1326@gmail.com>
2025-03-19 20:44:10 -0400

    condition

---
+++ Condition/class_9_conditionals.py
@@ -0,0 +1,147 @@
+
+
+''' Conditionals '''
+
+# Let's write a simple conditional that compares 2 values
+
+
+
+# Let's try a simple example with strings
+
+
+
+''' Your turn! :)
+Write a program that asks you what the temperature is. If it is over 60 degrees, the program will give the user a print statement saying, 'Another warm day' 
+'''
+
+
+
+'''
+
+Write some code that prints “This is odd” if the user inputs an odd number.
+(Hint: use an if statement)
+Example:
+User input: 7
+This is odd
+
+Let's work through some possible steps to solve this
+
+Step 1 Get User's Input
+Step 2 Evaluate Data and Deliver output via Conditional
+The question is this, how can we figure out if the value is even or odd? 
+
+'''
+
+
+
+
+
+
+'''
+Elif
+
+Add to your previous code so it prints “This is odd” if the user enters an odd number, and “This is even” if the user enters an even number.
+(Hint: add an elif statement)
+
+User input: 8
+This is even
+'''
+
+
+
+
+
+'''
+
+Add to your previous code so if the user enters something that isn’t an odd number or an even number, print “Unknown”. Be ready to troubleshoot our datatypes!
+(Hint: add an else statement)
+
+
+User input: 9
+This is odd
+
+User input: 9.2
+Unknown
+
+'''
+
+
+
+
+
+
+'''
+
+Write some code that takes in a string from the user and prints whether the string is a number, if it is a word, or something else.
+
+Examples:
+User input: 7
+This is a number
+
+User input: abcde
+This is a word
+
+User input: 7!ab5
+This is something else
+
+'''
+
+
+
+
+# Chaining Conditionals code results
+
+# result - it is hot outside
+
+
+
+# result - evaluated separately and multiple of them could be run
+
+
+
+
+
+# Logical operators
+# and returns true if they are both true
+
+
+# or returns true if either one of them is true
+
+
+# not returns the opposite
+
+
+# Order of Operations with logical operators
+
+
+
+
+# nested conditionals
+num = 5
+
+
+
+
+
+
+'''
+You’re working on a project to develop a login system for a website. The website requires the user to enter a username and password to log in. Write a Python program that checks whether the user entered the correct username and password.
+Create two variables called username and password and set them to any valid string values.
+Prompt the user to enter their username and password using the input() function.
+Use conditionals and logical operators to check whether the username and password entered by the user match the username and password variables.
+If they match, print “Login successful.” If they don’t, print “Incorrect username or password.”
+
+Follow the requirements, nothing more, nothing less. 
+'''
+
+
+
+
+
+
+'''
+pandas is a data manipulation package in Python for tabular data. That is, data in the form of rows and columns, also known as DataFrames. Intuitively, you can think of a DataFrame as an Excel sheet. 
+
+https://www.datacamp.com/tutorial/pandas
+'''
+
